3fd2bacf6ceea449a7efac7545dad6bfd360a823
TealNerd <jacobsokora@gmail.com>
2015-06-28 23:22:57 -0500

    Fixed waypoints not rendering outside render distance
    also converted back to direct GL calls as using the GLStateManager was
    screwing with opacity and didn't seem to provide much benefit. I may
    revisit this later

--- src/main/java/com/biggestnerd/civradar/RenderHandler.java
+++ src/main/java/com/biggestnerd/civradar/RenderHandler.java
@@ -26,6 +26,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
 
+import org.lwjgl.opengl.GL11;
+
 import com.biggestnerd.civradar.Config.NameLocation;
 
 public class RenderHandler extends Gui {
@@ -49,15 +51,15 @@ public class RenderHandler extends Gui {
 		if(event.type != RenderGameOverlayEvent.ElementType.CROSSHAIRS)
 			return;
 		if(config.isDubstepMode()) {
-			GLUtils.glPushMatrix();
-			GLUtils.glScalef(2.0f, 2.0f, 2.0f);
+			GL11.glPushMatrix();
+			GL11.glScalef(2.0f, 2.0f, 2.0f);
 			ScaledResolution res = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
 			int halfWidth = res.getScaledWidth() / 4;
 			int stringWidth = mc.fontRendererObj.getStringWidth("Dubstep Mode Enabled");
 			int height = res.getScaledHeight() / 8;
 			mc.fontRendererObj.drawStringWithShadow("Dubstep Mode Enabled", halfWidth - (stringWidth / 2), height, dubstepColor.getRGB());
-			GLUtils.glScalef(1.0f, 1.0f, 1.0f);
-			GLUtils.glPopMatrix();
+			GL11.glScalef(1.0f, 1.0f, 1.0f);
+			GL11.glPopMatrix();
 		}
 		if(config.isEnabled()) {
 			drawRadar();
@@ -118,83 +120,83 @@ public class RenderHandler extends Gui {
 		radarScale = config.getRadarScale();
 		ScaledResolution res = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
 		int width = res.getScaledWidth();
-		GLUtils.glPushMatrix();
-		GLUtils.glTranslatef(width - (65 * radarScale) + (config.getRadarX()), (65 * radarScale) + (config.getRadarY()), 0.0F);
-		GLUtils.glScalef(1.0F, 1.0F, 1.0F);
+		GL11.glPushMatrix();
+		GL11.glTranslatef(width - (65 * radarScale) + (config.getRadarX()), (65 * radarScale) + (config.getRadarY()), 0.0F);
+		GL11.glScalef(1.0F, 1.0F, 1.0F);
 		if(config.isRenderCoordinates()) {
 			String coords = "(" + (int) mc.thePlayer.posX + "," + (int) mc.thePlayer.posY + "," + (int) mc.thePlayer.posZ + ")";
 			mc.fontRendererObj.drawStringWithShadow(coords, -(mc.fontRendererObj.getStringWidth(coords) / 2), 65 * radarScale, 14737632);
 		}
-		GLUtils.glScalef(radarScale, radarScale, radarScale);
-		GLUtils.glRotatef(-mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
+		GL11.glScalef(radarScale, radarScale, radarScale);
+		GL11.glRotatef(-mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
 		drawCircle(0, 0, 63.0D, radarColor, true);
-		GLUtils.glLineWidth(2.0F);
+		GL11.glLineWidth(2.0F);
 		drawCircle(0, 0, 63.0D, radarColor, false);
-		GLUtils.glLineWidth(1.0F);
+		GL11.glLineWidth(1.0F);
 		
 		if(pingDelay > 0) {
 			drawCircle(0, 0, 63.0D - pingDelay, radarColor, false);
 		}
-		GLUtils.glLineWidth(2.0F);
-		GLUtils.glDisable(GLUtils.GL_TEXTURE_2D);
-		GLUtils.glDisable(GLUtils.GL_LIGHTING);
-		GLUtils.glBegin(1);
-		GLUtils.glColor4f(radarColor.getRed() / 255.0F, radarColor.getGreen() / 255.0F, radarColor.getBlue() / 255.0F, config.getRadarOpacity() + 0.5F);
-		GLUtils.glVertex2d(0.0D, -63.0D);
-		GLUtils.glVertex2d(0.0D, 63.0D);
-		GLUtils.glVertex2d(-63.0D, 0.0D);
-		GLUtils.glVertex2d(63.0D, 0.0D);
-		GLUtils.glVertex2d(-44.5D, -44.5D);
-		GLUtils.glVertex2d(44.5D, 44.5D);
-		GLUtils.glVertex2d(-44.5D, 44.5D);
-		GLUtils.glVertex2d(44.5D, -44.5D);
-		GLUtils.glEnd();
-		GLUtils.glDisable(GLUtils.GL_BLEND);
-		GLUtils.glEnable(GLUtils.GL_TEXTURE_2D);
+		GL11.glLineWidth(2.0F);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(GL11.GL_LIGHTING);
+		GL11.glBegin(1);
+		GL11.glColor4f(radarColor.getRed() / 255.0F, radarColor.getGreen() / 255.0F, radarColor.getBlue() / 255.0F, config.getRadarOpacity() + 0.5F);
+		GL11.glVertex2d(0.0D, -63.0D);
+		GL11.glVertex2d(0.0D, 63.0D);
+		GL11.glVertex2d(-63.0D, 0.0D);
+		GL11.glVertex2d(63.0D, 0.0D);
+		GL11.glVertex2d(-44.5D, -44.5D);
+		GL11.glVertex2d(44.5D, 44.5D);
+		GL11.glVertex2d(-44.5D, 44.5D);
+		GL11.glVertex2d(44.5D, -44.5D);
+		GL11.glEnd();
+		GL11.glDisable(GL11.GL_BLEND);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
 		
 		drawRadarIcons();
 		
-		GLUtils.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
+		GL11.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
 		
 		drawTriangle(0, 0, Color.WHITE);
-		GLUtils.glScalef(2.0F, 2.0F, 2.0F);
-		GLUtils.glPopMatrix();
+		GL11.glScalef(2.0F, 2.0F, 2.0F);
+		GL11.glPopMatrix();
 	}
 	
 	private void drawCircle(int x, int y, double radius, Color c, boolean filled) {
-		GLUtils.glEnable(3042);
-		GLUtils.glDisable(GLUtils.GL_TEXTURE_2D);
-		GLUtils.glEnable(2848);
-		GLUtils.glBlendFunc(770, 771);
-		GLUtils.glColor4f(c.getRed() / 255.0F, c.getGreen() / 255.0F, c.getBlue() / 255.0F, filled ? config.getRadarOpacity() : config.getRadarOpacity() + 0.5F);
-		GLUtils.glBegin(filled ? 6 : 2);
+		GL11.glEnable(3042);
+		GL11.glDisable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(2848);
+		GL11.glBlendFunc(770, 771);
+		GL11.glColor4f(c.getRed() / 255.0F, c.getGreen() / 255.0F, c.getBlue() / 255.0F, filled ? config.getRadarOpacity() : config.getRadarOpacity() + 0.5F);
+		GL11.glBegin(filled ? 6 : 2);
 		for (int i = 0; i <= 360; i++) {
 			double x2 = Math.sin(i * Math.PI / 180.0D) * radius;
 			double y2 = Math.cos(i * Math.PI / 180.0D) * radius;
-			GLUtils.glVertex2d(x + x2, y + y2);
+			GL11.glVertex2d(x + x2, y + y2);
 		}
-		GLUtils.glEnd();
-		GLUtils.glDisable(2848);
-		GLUtils.glEnable(GLUtils.GL_TEXTURE_2D);
-		GLUtils.glDisable(3042);
+		GL11.glEnd();
+		GL11.glDisable(2848);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glDisable(3042);
 	}
 	
 	private void drawTriangle(int x, int y, Color c) {
-		GLUtils.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-		GLUtils.glColor4f(c.getRed() / 255.0F, c.getGreen() / 255.0F, c.getBlue() / 255.0F, 1.0F);
-		GLUtils.glEnable(3042);
-		GLUtils.glDisable(3553);
-		GLUtils.glEnable(2848);
-		GLUtils.glBlendFunc(770, 771);
-		GLUtils.glBegin(4);
-		GLUtils.glVertex2d(x, y + 3);
-		GLUtils.glVertex2d(x + 3, y - 3);
-		GLUtils.glVertex2d(x - 3, y - 3);
-		GLUtils.glEnd();
-		GLUtils.glDisable(2848);
-		GLUtils.glEnable(3553);
-		GLUtils.glDisable(3042);
-		GLUtils.glRotatef(-180.0F, 0.0F, 0.0F, 1.0F);
+		GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+		GL11.glColor4f(c.getRed() / 255.0F, c.getGreen() / 255.0F, c.getBlue() / 255.0F, 1.0F);
+		GL11.glEnable(3042);
+		GL11.glDisable(3553);
+		GL11.glEnable(2848);
+		GL11.glBlendFunc(770, 771);
+		GL11.glBegin(4);
+		GL11.glVertex2d(x, y + 3);
+		GL11.glVertex2d(x + 3, y - 3);
+		GL11.glVertex2d(x - 3, y - 3);
+		GL11.glEnd();
+		GL11.glDisable(2848);
+		GL11.glEnable(3553);
+		GL11.glDisable(3042);
+		GL11.glRotatef(-180.0F, 0.0F, 0.0F, 1.0F);
 	}
 	
 	private void drawRadarIcons() {
@@ -237,63 +239,63 @@ public class RenderHandler extends Gui {
 	}
 	
 	private void renderItemIcon(int x, int y, ItemStack item) {
-		GLUtils.glPushMatrix();
-		GLUtils.glScalef(0.5F, 0.5F, 0.5F);
-		GLUtils.glTranslatef(x +1, y +1, 0.0F);
-		GLUtils.glColor4f(1.0F, 1.0F, 1.0F, config.getRadarOpacity() + 0.5F);
-		GLUtils.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
+		GL11.glPushMatrix();
+		GL11.glScalef(0.5F, 0.5F, 0.5F);
+		GL11.glTranslatef(x +1, y +1, 0.0F);
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, config.getRadarOpacity() + 0.5F);
+		GL11.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
 		mc.getRenderItem().renderItemIntoGUI(item, -8, -8);
-		GLUtils.glTranslatef(-x -1, -y -1, 0.0F);
-		GLUtils.glScalef(2.0F, 2.0F, 2.0F);
-		GLUtils.glDisable(2896);
-		GLUtils.glPopMatrix();
+		GL11.glTranslatef(-x -1, -y -1, 0.0F);
+		GL11.glScalef(2.0F, 2.0F, 2.0F);
+		GL11.glDisable(2896);
+		GL11.glPopMatrix();
 	}
 	
 	private void renderPlayerHeadIcon(int x, int y, EntityOtherPlayerMP player) throws Exception {
-		GLUtils.glColor4f(1.0F, 1.0F, 1.0F, config.getRadarOpacity() + 0.5F);
-		GLUtils.glEnable(3042);
-		GLUtils.glPushMatrix();
-		GLUtils.glScalef(0.5F, 0.5F, 0.5F);
-		GLUtils.glTranslatef(x + 1, y + 1, 0.0F);
-		GLUtils.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, config.getRadarOpacity() + 0.5F);
+		GL11.glEnable(3042);
+		GL11.glPushMatrix();
+		GL11.glScalef(0.5F, 0.5F, 0.5F);
+		GL11.glTranslatef(x + 1, y + 1, 0.0F);
+		GL11.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
 		mc.getTextureManager().bindTexture(new ResourceLocation("civRadar/icons/player.png"));
 		drawModalRectWithCustomSizedTexture(-8, -8, 0, 0, 16, 16, 16, 16);
-		GLUtils.glTranslatef(-x -1, -y -1, 0.0F);
-		GLUtils.glScalef(2.0F, 2.0F, 2.0F);
-		GLUtils.glDisable(2896);
-		GLUtils.glDisable(3042);
-		GLUtils.glPopMatrix();
+		GL11.glTranslatef(-x -1, -y -1, 0.0F);
+		GL11.glScalef(2.0F, 2.0F, 2.0F);
+		GL11.glDisable(2896);
+		GL11.glDisable(3042);
+		GL11.glPopMatrix();
 		if(config.isPlayerNames()) {
-			GLUtils.glPushMatrix();
-			GLUtils.glScalef(0.5F, 0.5F, 0.5F);
-			GLUtils.glTranslatef(x, y, 0.0F);
-			GLUtils.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
-			GLUtils.glTranslatef(-x - 8, -y, 0.0F);
+			GL11.glPushMatrix();
+			GL11.glScalef(0.5F, 0.5F, 0.5F);
+			GL11.glTranslatef(x - 8, y, 0.0F);
+			GL11.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
+			GL11.glTranslatef(-x, -y, 0.0F);
 			String playerName = player.getName();
 			if(config.isExtraPlayerInfo()) {
 				playerName += " (" + (int) mc.thePlayer.getDistanceToEntity(player) + "m)(Y" + (int) player.posY + ")";
 			}
 			int yOffset = config.getNameLocation() == NameLocation.below ? 10 : -10;
 			drawCenteredString(mc.fontRendererObj, playerName, x + 8, y + yOffset, Color.WHITE.getRGB());
-			GLUtils.glScalef(2.0F, 2.0F, 2.0F);
-			GLUtils.glPopMatrix();
+			GL11.glScalef(2.0F, 2.0F, 2.0F);
+			GL11.glPopMatrix();
 		}
 	}
 	
 	private void renderIcon(int x, int y, ResourceLocation resource) {
 		mc.getTextureManager().bindTexture(resource);
-		GLUtils.glColor4f(1.0F, 1.0F, 1.0F, config.getRadarOpacity() + 0.5F);
-		GLUtils.glEnable(3042);
-		GLUtils.glPushMatrix();
-		GLUtils.glScalef(0.5F, 0.5F, 0.5F);
-		GLUtils.glTranslatef(x + 1, y + 1, 0.0F);
-		GLUtils.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, config.getRadarOpacity() + 0.5F);
+		GL11.glEnable(3042);
+		GL11.glPushMatrix();
+		GL11.glScalef(0.5F, 0.5F, 0.5F);
+		GL11.glTranslatef(x + 1, y + 1, 0.0F);
+		GL11.glRotatef(mc.thePlayer.rotationYaw, 0.0F, 0.0F, 1.0F);
 		drawModalRectWithCustomSizedTexture(-8, -8, 0, 0, 16, 16, 16, 16);
-		GLUtils.glTranslatef(-x -1, -y -1, 0.0F);
-		GLUtils.glScalef(2.0F, 2.0F, 2.0F);
-		GLUtils.glDisable(2896);
-		GLUtils.glDisable(3042);
-		GLUtils.glPopMatrix();
+		GL11.glTranslatef(-x -1, -y -1, 0.0F);
+		GL11.glScalef(2.0F, 2.0F, 2.0F);
+		GL11.glDisable(2896);
+		GL11.glDisable(3042);
+		GL11.glPopMatrix();
 	}
 	
 	private void renderWaypoint(Waypoint point, RenderWorldLastEvent event) {
@@ -301,6 +303,7 @@ public class RenderHandler extends Gui {
 		Color c = point.getColor();
 		float partialTickTime = event.partialTicks;
 		double distance = point.getDistance(mc);
+		int maxView = mc.gameSettings.renderDistanceChunks * 22;
 		if(distance <= config.getMaxWaypointDistance() || config.getMaxWaypointDistance() < 0) {
 			FontRenderer fr = mc.fontRendererObj;
 			Tessellator tess = Tessellator.getInstance();
@@ -315,24 +318,32 @@ public class RenderHandler extends Gui {
 			float displayY = (float)point.getY() + 1.3f - playerY;
 			float displayZ = (float)point.getZ() - playerZ;
 			
-			float scale = (float) (Math.max(2, distance /5) * 0.0185f);
+			//z is what would typically be y here btw
+			//my friend laura provided moral support
+			if(distance > maxView) {
+				float slope = displayZ / displayX;
+				displayX = -((float)Math.sqrt(Math.pow(maxView, 2) / (1 + Math.pow(slope, 2))));
+				displayZ = slope * displayX;
+			}
 			
-			GLUtils.glColor4f(1f, 1f, 1f, 1f);
-			GLUtils.glPushMatrix();
-			GLUtils.glTranslatef(displayX, displayY, displayZ);
-			GLUtils.glRotatef(-rm.playerViewY, 0.0F, 1.0F, 0.0F);
-			GLUtils.glRotatef(rm.playerViewX, 1.0F, 0.0F, 0.0F);
-			GLUtils.glScalef(-scale, -scale, scale);
-			GLUtils.glDisable(GLUtils.GL_LIGHTING);
-			GLUtils.glDepthMask(false);
-			GLUtils.glDisable(GLUtils.GL_DEPTH_TEST);
-			GLUtils.glEnable(GLUtils.GL_BLEND);
-			GLUtils.glBlendFunc(GLUtils.GL_SRC_ALPHA, GLUtils.GL_ONE_MINUS_SRC_ALPHA);
+			float scale = 0.45F * (float)(Math.max(10.0D, distance) / 120);//old scaling math: (float) (Math.max(2, distance /5) * 0.0185f);
+			
+			GL11.glColor4f(1f, 1f, 1f, 1f);
+			GL11.glPushMatrix();
+			GL11.glTranslatef(displayX, displayY, displayZ);
+			GL11.glRotatef(-rm.playerViewY, 0.0F, 1.0F, 0.0F);
+			GL11.glRotatef(rm.playerViewX, 1.0F, 0.0F, 0.0F);
+			GL11.glScalef(-scale, -scale, scale);
+			GL11.glDisable(GL11.GL_LIGHTING);
+			GL11.glDepthMask(false);
+			GL11.glDisable(GL11.GL_DEPTH_TEST);
+			GL11.glEnable(GL11.GL_BLEND);
+			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			
 			name += " (" + (int)distance + "m)";
 			int width = fr.getStringWidth(name);
 			int height = 10;
-			GLUtils.glDisable(GLUtils.GL_TEXTURE_2D);
+			GL11.glDisable(GL11.GL_TEXTURE_2D);
 			wr.startDrawingQuads();
 			int stringMiddle = width / 2;
 			wr.setColorRGBA_F(c.getRed() / 255.0F, c.getGreen() / 255.0F, c.getBlue() / 255.0F, config.getWaypointOpcaity());
@@ -341,13 +352,13 @@ public class RenderHandler extends Gui {
 			wr.addVertex(stringMiddle + 1, 1 + height, 0.0D);
 			wr.addVertex(stringMiddle + 1,  -1, 0.0D);
 			tess.draw();
-			GLUtils.glEnable(GLUtils.GL_TEXTURE_2D);
+			GL11.glEnable(GL11.GL_TEXTURE_2D);
 			
 			fr.drawString(name, -width / 2, 1, Color.WHITE.getRGB());
-			GLUtils.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-			GLUtils.glDepthMask(true);
-			GLUtils.glEnable(GLUtils.GL_DEPTH_TEST);
-			GLUtils.glPopMatrix();
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			GL11.glDepthMask(true);
+			GL11.glEnable(GL11.GL_DEPTH_TEST);
+			GL11.glPopMatrix();
 		}
 	}
 }
